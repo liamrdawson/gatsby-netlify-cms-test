65dc46462102ed7a50830e2d32f506899ad463dd
Liam Dawson <liamrdawson@gmail.com>
2020-02-22 23:36:44 +0000

    rearrange config to add 'gatsby-plugin-sharp',     'gatsby-transformer-sharp' to top of config list

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
@@ -29,8 +31,6 @@ module.exports = {
         name: 'images',
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
