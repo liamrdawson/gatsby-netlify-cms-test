1e0c2057fd1df2bbe7ea7981e8e1edc4843cba1f
Liam Dawson <liamrdawson@gmail.com>
2020-02-27 19:40:06 +0000

    change blog to News

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,26 +67,14 @@ const Navbar = class extends React.Component {
                 Products
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                News & Events
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+
             </div>
           </div>
         </div>
