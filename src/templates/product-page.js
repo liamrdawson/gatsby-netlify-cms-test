2e9ef1484743a026c5d8a0a124f905f4efdea907
Liam Dawson <liamrdawson@gmail.com>
2020-02-29 19:41:14 +0000

    fix FullWidthImageContainer on product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -96,15 +96,27 @@ export const ProductPageTemplate = ({
                 </div>
               </div>
               <Testimonials testimonials={testimonials} />
-              <FullWidthImageContainer
-                style={{
-                  backgroundImage: `url(${
-                    fullImage.childImageSharp
-                      ? fullImage.childImageSharp.fluid.src
-                      : fullImage
-                  })`,
-                }}
-              />
+              </div>
+          </div>
+        </div>
+      </div>
+      <FullWidthImageContainer
+        style={{
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+          })`,
+        }}
+      />
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-7 is-offset-1">
+              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
               <h2 className="has-text-weight-semibold is-size-2">
                 {pricing.heading}
               </h2>
