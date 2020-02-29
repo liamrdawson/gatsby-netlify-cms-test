98cf2dcf0390eb6f4c489829cdddb2b8a36e6086
Liam Dawson <liamrdawson@gmail.com>
2020-02-29 20:05:22 +0000

    convert Testimonials.js to styled component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
+import styled from 'styled-components'
+
+const Message = styled.article`
+  :not(:last-child) {
+    margin-bottom: 1.5rem;
+  }
+  background-color: white;
+  border-radius: 4px;
+  font-size: 1rem;
+  div {
+    border-color: #dbdbdb;
+    border-radius: 4px;
+    border-style: solid;
+    border-width: 0 0 0 4px;
+    color: #4a4a4a;
+    padding: 1.25em 1.5em;
+  }
+`;
 
 const Testimonials = ({ testimonials }) => (
   <div>
     {testimonials.map(testimonial => (
-      <article key={v4()} className="message">
-        <div className="message-body">
+      <Message key={v4()} >
+        <div>
           {testimonial.quote}
           <br />
           <cite> – {testimonial.author}</cite>
         </div>
-      </article>
+      </Message>
     ))}
   </div>
 )
