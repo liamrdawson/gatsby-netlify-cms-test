9f640a55a98aea8dc76e7da14512c335191f90b6
Liam Dawson <liamrdawson@gmail.com>
2020-03-02 22:54:51 +0000

    convert thanks.js to styled component

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import styled from 'styled-components'
+import Section from '../../components/containers/Section'
+import Container from '../../components/containers/Container'
+
+const Content = styled.div`
+  h1 {
+    font-size: 2em;
+    margin-bottom: 0.5em;
+    color: #363636;
+    font-weight: 600;
+    line-height: 1.125;
+  }
+`;
 
 export default () => (
   <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
+    <Section>
+      <Container>
+        <Content>
           <h1>Thank you!</h1>
           <p>This is a custom thank you page for form submissions</p>
-        </div>
-      </div>
-    </section>
+        </Content>
+      </Container>
+    </Section>
   </Layout>
 )
